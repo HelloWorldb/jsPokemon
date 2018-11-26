73b32d609ec4bec50069efbd826eb77378ced473
helloworldb <miroh31894@gmail.com>
2018-11-26 16:48:43 +0900

    hihi

--- history.js
+++ history.js
@@ -11,3 +11,12 @@ console.log(fact(130))
 var sym1 = Symbol("mypet1")
 var sym2 = Symbol("mypet2")
 console.log(sym1.toString())
+
+var card ={"mine":"Spade A","yours":"Spade B"};
+card["third"] = "Spade 9";
+console.log(card);
+var a=new Date();
+console.log('${a.getDate()}');
+//console.log("${card["mine"]}");
+delete card["third"];
+console.log(card);
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var card ={"mine":"Spade A","yours":"Spade B"};
 card["third"] = "Spade 9";
 console.log(card);
-var a=1;
-console.log('${a}');
+var a=new Date();
+console.log('${a.getDate()}');
 //console.log("${card["mine"]}");
 delete card["third"];
 console.log(card);
